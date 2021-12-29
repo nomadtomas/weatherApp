b1c9d7973bf16982d0156246c15361ce81b776ab
Your Name <tomas.resendiz@icloud.com>
2021-12-29 15:40:44 -0600

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,11 @@
 console.log('Client side javascript file is loaded!')
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
 
-
-// fetch('http://localhost:3000/weather?address=Austin').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log("this is an error")
-//         } else {
-//             console.log(data.forecast)
-//             console.log(data.address)
-//         }
-//     })
-// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messagesOne = document.querySelector('#message-1')
 const messagesTwo = document.querySelector('#message-2')
-
-// messagesOne.textContent = city
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -32,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messagesOne.textContent = 'Loading...'
     messagesTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messagesOne.textContent = error
